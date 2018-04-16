2f408757e831e92c40bf4038e8f7e23843e0c9f5
Ashish Singh <singhashish4000@gmail.com>
2018-04-16 18:19:39 +0530

    Added route for all posts

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
    root 'welcome#index'
 
+   get 'links' => 'links#index', as: :all_links 
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
